3dbda97ff6ae01536068981ae608cff3bae36cfd
raganork-md <msanu9899@gmail.com>
2026-04-25 23:02:57 +0530

    Delete plugins/ping.js

--- plugins/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Ping command to check latency',
-    execute: async (client, message) => {
-        const latency = Date.now() - message.createdTimestamp;
-        await message.channel.send(`Pong! Latency is ${latency}ms.`);
-    },
-    pattern: /^\.ping$/i
-};
